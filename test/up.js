b84b4945cdc6b3b0a501c8345453569549b9fbff
Guillermo Rauch <rauchg@gmail.com>
2012-01-31 14:21:20 -0800

    Added suicide test.

--- test/up.js
+++ test/up.js
@@ -136,4 +136,42 @@ describe('up', function () {
     }
   });
 
+  it('should suicide workers if master dies', function (done) {
+    // utility to check whether a pid is alive
+    // https://raw.github.com/visionmedia/monit.js/master/lib/utils.js
+    function alive (pid) {
+      try {
+        process.kill(pid, 0);
+        return true;
+      } catch (err) {
+        return false;
+      }
+    };
+
+    var proc;
+
+    // the spawn process will start an up server with 1 worker and
+    // will send us the pid over a net channel
+    net.createServer(function (conn) {
+      conn.setEncoding('utf8');
+      conn.on('data', function (pid) {
+        expect(alive(pid)).to.be(true);
+
+        // kill master
+        proc.kill('SIGHUP');
+
+        // since the ping interval is set to 15ms, we try in 30
+        setTimeout(function () {
+          expect(alive(pid)).to.be(false);
+          done();
+        }, 30);
+      });
+    }).listen(6003, onListen);
+
+    function onListen () {
+      // create a child process (master) we'll kill later
+      proc = child_process.spawn('node', [__dirname + '/child.js']);
+    }
+  });
+
 });
